8dbc02ea820b3cd95a67008703122f43764474ef
adzafar <adeel.zafar@flipkart.com>
2014-08-08 04:14:51 +0530

    went fullscreen

--- app/src/main/java/com/flipkart/adeelzafar/finding_nemo/Main.java
+++ app/src/main/java/com/flipkart/adeelzafar/finding_nemo/Main.java
@@ -17,6 +17,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 
@@ -39,6 +41,9 @@ public class Main extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         // Create the adapter that will return a fragment for each of the three
