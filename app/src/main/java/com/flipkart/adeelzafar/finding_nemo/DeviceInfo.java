7ce80cb4162fb638e24d203ed2084424e6a04f66
Saurabh Agrawal <saurabh.agrawal@flipkart.com>
2014-08-08 12:51:29 +0530

    missed file as part of List Manager

---
+++ app/src/main/java/com/flipkart/adeelzafar/finding_nemo/DeviceInfo.java
@@ -0,0 +1,15 @@
+package com.flipkart.adeelzafar.finding_nemo;
+
+/**
+ * Created by saurabh.agrawal on 08/08/14.
+ */
+public class DeviceInfo {
+
+    public String mName;
+    public String mAddress;
+
+    public DeviceInfo(String name, String address) {
+        mName = name;
+        mAddress = address;
+    }
+}
